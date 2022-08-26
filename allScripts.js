be1c4a0605b957c99239226406a59d4ea2031e4a
Sabythedestroyer <101746332+Sabythedestroyer@users.noreply.github.com>
2022-08-26 14:58:07 -0400

    Fixes the relative path of the navbar

--- allScripts.js
+++ allScripts.js
@@ -1,5 +1,10 @@
-$(document).ready(function() {
+/// A JavaScript Function to toggle in and out of menu_show and menu hide classes 
+
+$(Document).ready(function() {
 	$("body").on('click', '.top', function() {
-		$("nav.menu").toggleClass("menu_show");
+		$("nav.menu").toggleClass("menu_hide");
 	});
+	$("body").on('click','.top',function(){
+		$("nav.menu").toggleClass("menu_show")
+	})
 });
